5200cefd5acfbea8f0cdb5c3f6b491e983b33d4d
Robert N <robert.newton@tryspot.co>
2015-02-22 21:05:39 -0600

    Initial commit

---
+++ InventoryManagement-BT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InventoryManagement_BT.Startup))]
+namespace InventoryManagement_BT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
